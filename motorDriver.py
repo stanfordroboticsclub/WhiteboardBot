c1ef3a74b35029dac26ee837c6e0fa1bf56958f6
Hannah <hannahgailpw@gmail.com>
2019-04-13 00:05:02 +0000

    wrote more stuff for the class

--- motorDriver.py
+++ motorDriver.py
@@ -6,13 +6,19 @@ class Motor:
         self.serial = serial.Serial(port, 115200)
         pass
 
+    def step(self, numSteps):
+	for i in range(numSteps):
+	    self.serial.write("s")
+
     def drive(self, pos):
         pass
 
 
 
-left_motor = Motor("/dev/tty/ACM0")
-right_motor = Motor()
+left_motor = Motor("/dev/ttyACM0")
+right_motor = Motor("/dev/ttyACM1")
 
+for i in range(50):
+    left_motor.step(1)
+    right_motor.step(1)
 
-right_motor.drive(1)
