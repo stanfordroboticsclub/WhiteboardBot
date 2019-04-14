26bac8d83e67d559c24a1154347ca82ff6b02587
Hannah <hannahgailpw@gmail.com>
2019-04-14 21:58:28 +0000

    added a bunch of weird delays to allow movement

--- motorDriver.py
+++ motorDriver.py
@@ -4,27 +4,37 @@ import time
 class Motor:
     def __init__(self, port):
         self.serial = serial.Serial(port, 115200)
-        self.serial.write("nxr0")
+        self.serial.write("x")
+        self.serial.write("y")
+        self.serial.write("r0")
         self.angle = 0
+        print("motor initialized")
 
     def move(self, angleChange):
         self.angle = self.angle + angleChange
         self.serial.write("r")
-        self.serial.write(self.angle)        
+        self.serial.write(str(self.angle))
+        print("moving %d" %(angleChange))
 
     def drive(self, pos):
         pass
 
 
 left_motor = Motor("/dev/ttyACM0")
+
+time.sleep(1)
+
 right_motor = Motor("/dev/ttyACM1")
 
 time.sleep(5)
 
 #figure out how to read all lines
-move(left_motor, 360)
-move(right_motor, 360)
+left_motor.move(360)
+time.sleep(.1)
+right_motor.move(360)
 
-move(left_motor, -360)
-move(right_motor, -360)
+left_motor.move(-360)
+right_motor.move(-360)
+
+
 
