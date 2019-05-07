4ec5add25639c59dda48b0c1a16715c7e2287c2e
hannahpw <49597484+hannahpw@users.noreply.github.com>
2019-05-06 18:32:27 -0700

    Update motorDriver.py
    took out running code

--- motorDriver.py
+++ motorDriver.py
@@ -38,36 +38,3 @@ class Motor:
 
     def drive(self, pos):
         pass
-
-
-
-#start by initializing motors
-left_motor = Motor("/dev/ttyACM0")
-right_motor = Motor("/dev/ttyACM1")
-
-#vars for overall position
-y = 0
-x = 0
-
-yCoor = float(input("Choose a new y coor (0 to quit) "))
-xCoor = float(input("Choose a new x coor (0 to quit) "))
-
-#moves in y-direction to user-designated position
-while yCoor != 0 and xCoor != 0: #need a better sentinel value... idk what would make most sense
-    #figure out how to do x coor using string length thingy
-    dy = (yCoor - y) / 1.3 #circumference of pulley
-
-    changeLeft = -1
-    changeRight = 1
-
-    if dy < 0:
-        changeLeft *= -1
-        changeRight *= -1
-        dy *= -1
-    for i in range(int(dy * 360)):
-        left_motor.move(changeLeft)
-        right_motor.move(changeRight) 
-
-    print("Done moving!")
-    yCoor = float(input("Choose a new y coor (0 to quit) "))
-#    xCoor = float(input("Choose a new x coor (0 to quit) "))
