e13cfcc6baa774d97eb1f6335899f29645f5c15c
Alex Fu <afu625@stanford.edu>
2019-05-06 19:02:31 -0700

    first try at moveTo code

--- whiteBoardBot.py
+++ whiteBoardBot.py
@@ -1,35 +1,75 @@
 from motorDriver import Motor
+import math
+
+
+WIDTH = 1000 # constant which keeps track of width - distance between motors of canvas                   
+HEIGHT = 1000 # constant which keeps track of height of canvas                                          
+CIRCUMFERENCE = 1.3 # 13 millimeters                                                                     
+
+global_string_lengths = StringLengths();
+globalX = 0 # global variable to hold x coordinate of robot                                              
+globalY = 0 # global variable to hold y coordinate of robot 
+
+
+# Default is upper left corner
+class StringLengths:
+    def __init__(self):
+        self.left = 0
+        self.right = WIDTH
+
+
+class Rotations:
+    def __init__(self):
+        self.left = 0
+        self.right = 0
+
 
 class robot:
     def __init__(self, left_port, right_port):
         self.left_motor = Motor(left_port)
         self.right_motor = Motor(right_port)
 
-wbb = robot("/dev/ttyACM0", "/dev/ttyACM1") #WhiteBoard Bot
+    # function to calculate left and right string lengths given an x and a y coordinate.                
+    def coordinate_to_string_length(x, y):
+        string_pair = StringLengths()
+        string_pair.left = math.sqrt(math.pow(x,2) + math.pow(y,2))
+        string_pair.right = math.sqrt(math.pow(WIDTH,2) - 2*WIDTH*x + math.pow(x,2) + math.pow(y,2))
+        return string_pair
 
-#vars for overall position
-y = 0
-x = 0
+    # gets the string movement change between two string lengths                                       
+    def get_string_length_change(current_string_lengths, desired_string_lengths):
+        string_length_change = StringLengths()
+        string_length_change.left = desired_string_lengths.left - current_string_lengths.left
+        string_length_change.right = desired_string_lengths.right - current_string_lengths.right
+        return string_length_change
+
+    def get_rotations_in_degrees(current_string_lengths, desired_string_lengths):
+        strings_change = get_string_length_change(current_string_lengths, desired_string_lengths)
+        rotations_change = Rotations()
+        rotations_change.left = strings_change.left/CIRCUMFERENCE * 360
+        rotations_change.right = strings_change.right/CIRCUMFERENCE * 360
+        return rotations_change
+
+    def moveTo(xCord, yCord):
+        new_string_pair = coordinate_to_string_length(xCord, yCord)
+        string_pair_change = get_string_length_change(global_string_lengths, new_string_pair)
+        rotations_pair_change = get_rotations_in_degrees(global_string_lengths, new_string_pair)
+        self.left_motor.move(rotations_pair_change.left)
+        self.right_motor.move(rotations_pair_change.right)
+
+        globalX = xCord
+        globalY = yCord
+        global_string_lengths = new_string_pair
+
+wbb = robot("/dev/ttyACM0", "/dev/ttyACM1") #WhiteBoard Bot
 
 yCoor = float(input("Choose a new y coor (0 to quit) "))
 xCoor = float(input("Choose a new x coor (0 to quit) "))
 
 #moves in y-direction to user-designated position
 while yCoor != 0 and xCoor != 0: #need a better sentinel value... idk what would make most sense
-    #figure out how to do x coor using string length thingy
-    dy = (yCoor - y) / 1.3 #circumference of pulley
-
-    changeLeft = -1
-    changeRight = 1
-
-    if dy < 0:
-        changeLeft *= -1
-        changeRight *= -1
-        dy *= -1
-    for i in range(int(dy * 360)):
-        wbb.left_motor.move(changeLeft)
-        wbb.right_motor.move(changeRight) 
-
+    wbb.moveTo(xCoor, yCoor);
+    
     print("Done moving!")
     yCoor = float(input("Choose a new y coor (0 to quit) "))
-#    xCoor = float(input("Choose a new x coor (0 to quit) "))
+    xCoor = float(input("Choose a new x coor (0 to quit) "))
