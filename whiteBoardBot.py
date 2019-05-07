ce2bb34a5f9778a0713f2044fc54c525b6a3dec4
hannahpw <49597484+hannahpw@users.noreply.github.com>
2019-05-06 18:31:28 -0700

    Update whiteBoardBot.py
    moved over code from motordriver.py

--- whiteBoardBot.py
+++ whiteBoardBot.py
@@ -3,8 +3,33 @@ from motorDriver import motor
 class robot:
     def __init__(self, left_port, right_port):
         left_motor = Motor(left_port)
-        time.sleep(1)
         right_motor = Motor(right_port)
 
 wbb = robot("/dev/ttyACM0", "/dev/ttyACM1") #WhiteBoard Bot
 
+#vars for overall position
+y = 0
+x = 0
+
+yCoor = float(input("Choose a new y coor (0 to quit) "))
+xCoor = float(input("Choose a new x coor (0 to quit) "))
+
+#moves in y-direction to user-designated position
+while yCoor != 0 and xCoor != 0: #need a better sentinel value... idk what would make most sense
+    #figure out how to do x coor using string length thingy
+    dy = (yCoor - y) / 1.3 #circumference of pulley
+
+    changeLeft = -1
+    changeRight = 1
+
+    if dy < 0:
+        changeLeft *= -1
+        changeRight *= -1
+        dy *= -1
+    for i in range(int(dy * 360)):
+        left_motor.move(changeLeft)
+        right_motor.move(changeRight) 
+
+    print("Done moving!")
+    yCoor = float(input("Choose a new y coor (0 to quit) "))
+#    xCoor = float(input("Choose a new x coor (0 to quit) "))
